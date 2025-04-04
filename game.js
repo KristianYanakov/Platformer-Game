fd3286280b6f4b4a6221ebd26ac705bf1a16ddc3
KristianYanakov <christianyanakov@gmail.com>
2025-04-03 17:17:49 -0700

    starting game file

---
+++ game.js
@@ -0,0 +1,27 @@
+class GroundEntity{
+    constructor(x, y, sx, sy, image){
+        this.x = x
+        this.y = y
+        this.sx = sx
+        this.sy = sy
+        
+    }
+}
+function update() {
+
+}
+
+function draw() {
+    
+}
+
+function keyup(key) {
+    //console.log("Pressed", key);
+
+
+}
+
+function mouseup() {
+    //console.log("Mouse clicked at", mouseX, mouseY);
+
+}
